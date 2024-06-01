b2e755ff76f6b8403103d4496514c67d93578eca
Hecate-vv <wdobrodziej@gmail.com>
2024-06-01 18:33:15 +0200

    zad3

--- src/main/java/app/pplab7/Main.java
+++ src/main/java/app/pplab7/Main.java
@@ -26,6 +26,7 @@ public class Main extends Application {
         Button browseButton = new Button("Browse");
         browseButton.setOnAction(e -> browseDirectory());
         Button searchButton = new Button("Search");
+        searchButton.setOnAction(e -> searchFiles());
 
         HBox hBox = new HBox(10, directoryPathField, browseButton);
         VBox vBox = new VBox(10, hBox, searchField, searchButton);
@@ -46,6 +47,37 @@ public class Main extends Application {
             directoryPathField.setText(selectedDirectory.getAbsolutePath());
         }
     }
+
+    private void searchFiles() {
+        String directoryPath = directoryPathField.getText();
+
+        if (directoryPath.isEmpty()) {
+            resultArea.setText("Please provide a directory path.");
+            return;
+        }
+        File directory = new File(directoryPath);
+        if (!directory.isDirectory()) {
+            resultArea.setText("The provided path is not a directory.");
+            return;
+        }
+        StringBuilder results = new StringBuilder();
+        searchInDirectory(directory, results);
+        resultArea.setText(results.toString());
+        StringBuilder results = new StringBuilder();
+        listFilesInDirectory(directory, results);
+        resultArea.setText(results.toString());
+
+        private void listFilesInDirectory(File directory, StringBuilder results) {
+            File[] files = directory.listFiles();
+        }
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    results.append(file.getAbsolutePath()).append("\n");
+            }
+        }
+    }
+}
 
     public static void main(String[] args) {
         launch(args);
