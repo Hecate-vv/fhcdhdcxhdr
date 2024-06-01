6164ef3fbdea8ae13c08a0913587060ef405c011
Hecate-vv <wdobrodziej@gmail.com>
2024-06-01 18:10:23 +0200

    zad1

---
+++ src/main/java/app/pplab7/Main.java
@@ -0,0 +1,14 @@
+package app.pplab7;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
